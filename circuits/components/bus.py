80b4097cde7d4e8bd96abe7c650bdb988d2b60ce
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2023-03-16 18:51:14 +0530

    Added a reset method to the bus

--- circuits/components/bus.py
+++ circuits/components/bus.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:
 
 class Bus:
     def __init__(self, size: int = 8):
+        self.size = size
         self.__value = (0,) * size
 
     def __call__(self, value: Iterable[bool] = None):
@@ -20,8 +21,17 @@ class Bus:
         return self.__value
 
     def write(self, value: Iterable[bool]):
-        if len(value) != len(self.__value):
-            return
+        value_l, write_value_l = len(value), len(self.__value)
+
+        if value_l > write_value_l:
+            value = value[-(write_value_l):]
+            pass
+        elif value_l < write_value_l:
+            value = [0] * (write_value_l - value_l)
+            pass
 
         self.__value = value
         return self.__value
+
+    def reset(self):
+        self.write((0,) * self.size)
