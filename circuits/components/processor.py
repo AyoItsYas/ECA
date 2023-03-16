ecc5d2799daba9450e9524d325d19920a13dc83e
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2023-03-16 18:54:23 +0530

    Implemented few a instructions & added cycle hooks

--- circuits/components/processor.py
+++ circuits/components/processor.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import Callable
 
+import time
+
 from circuits.utils.formatting import bits_to_string
 
 from circuits.components.bus import Bus
@@ -16,46 +18,68 @@ class CPU:
         self,
         data_lines: Bus,
         address_lines: Bus,
-        clock_speed: float,
+        clock: float,
         *,
-        size: int = 8,
         logger: Callable[[str, str], None],
+        hooks_fetch: list[Callable] = None,
+        hooks_decode: list[Callable] = None,
+        hooks_execute: list[Callable] = None,
     ):
-        self.__log = logger
+        self.clock = clock
 
-        self.size = size
-        self.clock_speed = clock_speed
+        self.__log = logger
 
         self.__data_lines = data_lines
         self.__address_lines = address_lines
         self.__program_counter = Bus(len(address_lines))
 
-        self.__run_flag = True
+        self.__register_A = Bus()
+        self.__register_B = Bus()
+        self.__register_C = Bus()
+        self.__register_D = Bus()
 
         self.__instruction_set = {
             "0b00000000": self.NULL,
-            "0b00000001": self.JMP_,
-            "0b00000010": self.LDA_,
+            "0b00000001": self.JUMP,
+            "0b00000010": self.LOAD,
         }
 
+        self.__hooks_fetch = hooks_fetch
+        self.__hooks_decode = hooks_decode
+        self.__hooks_execute = hooks_execute
+
+        self.__circuit: Callable = lambda: None
+
     def fetch(self):
-        addr = self.__address_lines.read()
+        if self.__hooks_fetch:
+            [hook() for hook in self.__hooks_fetch]
+
         data = self.__data_lines.read()
+        addr = self.__address_lines.read()
         self.__log(f"FET {bits_to_string(addr, 'addr')} >>> {bits_to_string(data)}")
 
     def decode(self):
+        if self.__hooks_decode:
+            [hook() for hook in self.__hooks_decode]
+
         data = self.__data_lines.read()
         op_code = bits_to_string(data)
 
-        self.__circuit: Callable = self.__instruction_set.get(op_code, lambda x: None)
+        self.__circuit: Callable = self.__instruction_set.get(op_code, self.NULL)
 
         self.__log(
             f"DEC {op_code} ({bits_to_string(data, 'h')}) >>> {self.__circuit.__name__}"
         )
 
     def execute(self):
-        value = self.__circuit()
-        self.__log(f"EXE {self.__circuit.__name__} >>> {value}")
+        if self.__hooks_execute:
+            [hook() for hook in self.__hooks_execute]
+
+        self.__log(f"EXE {self.__circuit.__name__}")
+
+        self.__circuit()  # execute the insturction circuit
+
+        self.__address_lines.write(self.__program_counter.read())
 
     def increment_program(self):
         program_counter = self.__program_counter.read()
@@ -63,30 +87,72 @@ class CPU:
             program_counter, (0,) * len(program_counter) + (1,)
         )
 
-        if carry:
-            self.__program_counter.write((0,) * 3)
-        else:
-            self.__program_counter.write(new_program_counter)
+        new_program_counter = (
+            self.__program_counter.write((0,) * len(program_counter))
+            if carry
+            else new_program_counter
+        )
 
-        self.__data_lines.write((1,) * 8)
-        self.__address_lines.write((1, *self.__program_counter.read()))
+        self.__program_counter.write(new_program_counter)
 
     def cycle(self):
-        if not self.__run_flag:
-            return
+        cycle_s = time.perf_counter()
 
         self.fetch()
         self.decode()
         self.execute()
 
-    def reset(self):
-        self.__run_flag = False
+        cycle_e = time.perf_counter()
+
+        cycle_d = cycle_e - cycle_s
+
+        if cycle_d > self.clock:
+            self.__log(f"Falling behind clock! {cycle_d * 1000}ms", "WARN")
+        else:
+            time.sleep(self.clock - cycle_d)
+
+    def register_instruction(function):
+        def wrapper(self):
+            function(self)
+
+        x = wrapper
+        x.__name__ = function.__name__
+
+        return x
 
     def NULL(self):
-        return None
+        self.increment_program()
 
-    def JMP_(self):
-        return False
+    def JUMP(self):
+        self.__address_lines.write(self.__program_counter.read())
+        self.fetch()
 
-    def LDA_(self):
-        return False
+        self.__program_counter.write(self.__data_lines.read())
+
+        self.increment_program()
+
+    def LOAD(self):
+        self.increment_program()
+        self.__address_lines.write(self.__program_counter.read())
+        self.fetch()
+
+        data = self.__data_lines.read()
+        x, y = data[-2:]
+
+        if not x and not y:
+            register = self.__register_A
+        elif not x and y:
+            register = self.__register_B
+        elif x and not y:
+            register = self.__register_C
+        else:
+            register = self.__register_D
+
+        self.increment_program()
+        self.__address_lines.write(self.__program_counter.read())
+        self.fetch()
+
+        data = self.__data_lines.read()
+        self.__log(register.read())
+        register.write(data)
+        self.__log(register.read())
